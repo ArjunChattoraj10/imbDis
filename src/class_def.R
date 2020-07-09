9dc582981a9d7dd62fb5e37cc1c66489937e0e99
Arjun Chattoraj <arjunchattoraj10@gmail.com>
2020-07-09 02:31:02 -0400

    added f1 metric

--- src/class_def.R
+++ src/class_def.R
@@ -2,44 +2,82 @@
 
 library(pROC)
 
-simMetric = function(labels, pred.probs, case, bins = seq(0.1,0.5,0.1)){
+simMetric = function(orig.labels, pred.labels, pred.probs, 
+                     case, bins = seq(0.1,0.5,0.1)){
     
     # check values
-    param_check(labels, pred.probs, case, bins)
+    param_check(orig.labels, pred.labels, pred.probs, case, bins)
     
-    # initialize the class via list object
-    val = list(labels = labels, pred.probs = pred.probs, bins = bins, case = case)
-    attr(val, "class") = "simMetric" # sets the class
-    val
+    # initialize the class via list object using provided arguments
+    obj = list(orig.labels = orig.labels, pred.labels = pred.labels, 
+               pred.probs = pred.probs, bins = bins, case = case)
     
+    attr(obj, "class") = "simMetric" # sets the class
+    
+    # define control and add to list
+    obj$control = unique(pred.labels[pred.labels != case])
+    
+    # define standardized labels and add to list
+    obj$orig_01 = standardizeLabels(orig.labels, case)
+    obj$pred_01 = standardizeLabels(pred.labels, case)
+    
+    # define sample size and bin case sizes
+    obj$sample.size = calc_samplesize(obj$pred_01, bins)
+    obj$bin.caseSizes = round(bins*obj$sample.size)
+    
+    return(obj)
 }
 
-auc = function(obj) UseMethod("auc")
+param_check = function(orig.labels, pred.labels, pred.probs, case, bins){
+    # function to check if arguments of a simMetric object are valid
+    
+    if(length(unique(orig.labels)) != 2) 
+        stop('\'orig.labels\' may only have 2 distinct values.')
+    
+    if(length(unique(pred.labels)) != 2) 
+        stop('\'pred.labels\' may only have 2 distinct values.')
+    
+    if(!is.numeric(pred.probs))
+        stop('\'pred.probs\' contains non-numeric values.')
+    
+    if(length(orig.labels) != length(pred.labels))
+        stop('Length of \'orig.labels\' does not match length of \'pred.labels\'.')    
+    
+    if(any(pred.probs < 0 | pred.probs > 1)) 
+        stop('At least one element of \'pred.probs\' is less than 0 or greater than 1.')
+    
+    if(length(pred.labels) != length(pred.probs))
+        stop('Length of \'pred.labels\' does not match length of \'pred.probs\'.')
+    
+    if(!case %in% labels)
+        stop('Value of \'case\' is not present in \'labels\'.')
+    
+    if(!is.numeric(bins))
+        stop('\'bins\' is non-numeric.')
+    
+    if(any(bins < 0 | bins > 1)) 
+        stop('At least one element of \'bins\' is less than 0 or greater than 1.')
+    
+    if(any(bins == 0 | bins == 1))
+        stop('Elements of \'bin\' cannot be exactly 0 or 1.')
+}
 
-auc.simMetric = function(simMetric_obj){
-    # function takes in an object of class simMetric
-    # calculates the C-Statistic for specified frequency bins
-    
-    # define variables to reduce code clutter
-    labels = simMetric_obj$labels
-    pred.probs = simMetric_obj$pred.probs
-    case = simMetric_obj$case
-    bins = simMetric_obj$bins
-    
-    ## identify value of control
-    control = unique(labels[labels != case])
-    
-    ## set labels as case = 1 and control = 0
-    ## necessary to ensure proper functionality of roc()
+standardizeLabels = function(labels, case){
+    # converts a binary vector of labels  to a vector
+    # where all case values are 1 and controls are 0
     labels_01 = rep(0, length(labels))
     labels_01[labels == case] = 1
-    
+    return(labels_01)
+}
+standardiseLabels = standardizeLabels # alternate spelling
+
+calc_samplesize = function(labels_01, bins){
     ## total cases and controls in labels
     tot_cases = sum(labels_01)
     tot_controls = sum(labels_01 == 0)
     
     ## proportion of cases and controls in labels
-    prop_cases = mean(labels)
+    prop_cases = mean(labels_01)
     prop_control = 1 - prop_cases
     
     ## sample size calculations
@@ -47,7 +85,20 @@ auc.simMetric = function(simMetric_obj){
                              tot_cases/max(bins),        # use the cases procedure
                              tot_controls/max(1-bins)))  # else use the controls procedure
     
-    cases_list = round(bins*samp_size)
+    return(samp_size)
+}
+
+auc = function(obj) UseMethod("auc")
+auc.simMetric = function(obj){
+    # function takes in an object of class simMetric
+    # calculates the C-Statistic for specified frequency bins
+    
+    # define variables to reduce code clutter
+    pred.probs = obj$pred.probs
+    bins = obj$bins
+    labels_01 = obj$pred_01 # necessary for proper functionality of roc()
+    samp_size = obj$sample.size
+    cases_list = obj$bin.caseSizes
     
     ## subset labels and probs into only case and only not case
     
@@ -56,11 +107,11 @@ auc.simMetric = function(simMetric_obj){
     case_i = which(labels_01 == 1)
     
     ### subset labels and probs for case
-    labels_case = labels[case_i]
+    labels_case = labels_01[case_i]
     probs_case = pred.probs[case_i]
     
     ### subset labels and prons for not case
-    labels_notcase = labels[-case_i]
+    labels_control = labels_01[-case_i]
     probs_control = pred.probs[-case_i]
     
     ## compute returned values
@@ -79,7 +130,7 @@ auc.simMetric = function(simMetric_obj){
         
         # labels and probabilities organized as all elements corresponding to case
         # followed by all elements corresponding to control
-        labs = c(labels_case[sub_i_case], labels_notcase[sub_i_control])    
+        labs = c(labels_case[sub_i_case], labels_control[sub_i_control])    
         probs = c(probs_case[sub_i_case], probs_control[sub_i_control])
         
         # shuffle labels and predictions
@@ -99,57 +150,76 @@ auc.simMetric = function(simMetric_obj){
     return(data.frame(bins, auroc, n_samps))
 }
 
-param_check = function(labels, pred.probs, case, bins){
-    if(length(unique(labels)) != 2) 
-        stop('\'labels\' may only have 2 distinct values.')
+f1 = function(obj) UseMethod("f1")
+f1.simMetric = function(obj){
     
-    if(class(pred.probs) != "numeric")
-        stop('\'pred.probs\' contains non-numeric values.')
+    # standardized labels only
+    orig = obj$orig_01
+    pred = obj$pred_01
+    bins = obj$bins
+    samp_size = obj$sample.size
+    cases_list = obj$bin.caseSizes
     
-    if(any(pred.probs < 0 | pred.probs > 1)) 
-        stop('At least one element of \'pred.probs\' is less than 0 or greater than 1.')
+    ## subset labels into only case and only not case
     
-    if(length(labels) != length(pred.probs))
-        stop('Length of \'labels\' does not match length of \'pred.probs\'.')
+    ### indices with predicted case label
+    ### ensures correspondence of labels after subsetting
+    case_i = which(pred == 1)
     
-    if(!case %in% labels)
-        stop('Value of \'case\' is not present in \'labels\'.')
+    ### subset labels for case
+    orig_case = orig[case_i]
+    pred_case = pred[case_i]
     
-    if(class(bins) != "numeric")
-        stop('\'bins\' is non-numeric.')
+    ### subset labels for not case
+    orig_control = orig[-case_i]
+    pred_control = pred[-case_i]
     
-    if(any(bins < 0 | bins > 1)) 
-        stop('At least one element of \'bins\' is less than 0 or greater than 1.')
+    ## compute returned values
+    f1 = rep(0, length(bins))
+    n_samps = rep(0, length(bins))
     
-    if(any(bins == 0 | bins == 1))
-        stop('Elements of \'bin\' cannot be exactly 0 or 1.')
+    for(i in 1:length(bins)){
+        
+        # set up ROC calculations
+        n_cases = cases_list[i]
+        n_control = samp_size - n_cases
+        
+        # random sample of indices for case and non-case labels and probabilities
+        sub_i_case = sample(length(orig_case), n_cases)
+        sub_i_control = sample(length(orig_control), n_control)
+        
+        # labels and probabilities organized as all elements corresponding to case
+        # followed by all elements corresponding to control
+        orig_sub = c(orig_case[sub_i_case], orig_control[sub_i_control])    
+        pred_sub = c(pred_case[sub_i_case], pred_control[sub_i_control])
+        
+        # random shuffle not necessary as confusion matrix
+        # does not depend on order of entries
+        
+        # perform F1 calculation
+        
+        # pivot table -- confusion matrix for subsetted data
+        # rows for original, columns for predicted
+        pivot = table(orig_sub, pred_sub)
+        
+        # trueNeg = pivot[1,1]
+        truePos = pivot[2,2]
+        # falseNeg = pivot[2,1]
+        # falsePos = pivot[1,2]
+        
+        totOrigPos = sum(orig_sub)
+        totPredPos = sum(pred_sub)
+        
+        # precision and recall
+        prec = truePos/totPredPos
+        rec = truePos/totOrigPos
+        
+        # use F1 formula and save to vector
+        f1[i] = 2*prec*rec/(prec+rec)
+        n_samps[i] = length(pred_sub)
+    }
     
+    # return data frame
+    return(data.frame(bins, f1, n_samps))
 }
 
-# Testing
-
-## to ensure calls work
-SM1 = simMetric(c(1,1,0,1), c(0.1,0.2,0.3,0.4), 1, c(0.1,0.2,0.3,0.4))
-SM2 = simMetric(labels = c(1,1,0,1), pred.probs = c(0.1,0.2,0.3,0.4), case = 1)
-
-## real function calls
-
-set.seed(34)
-n = 1000
-labels = rbinom(n, 1, 0.2)
-
-# make realistic pred.probs
-pred.probs= rep(0, n)
-for(i in 1:n){
-    if(labels[i] == 0) pred.probs[i] = rnorm(1,0.25, 0.2)
-    else pred.probs[i] = rnorm(1,0.75, 0.2)
-}
-pred.probs[pred.probs < 0] = 0.05
-pred.probs[pred.probs > 1] = 0.95
-
-case = 1
-
-bins = c(0.1, 0.2, 0.3)
-
-SM3 = simMetric(labels, pred.probs, case)
-auc(SM3)
---
+++ src/class_test.R
@@ -0,0 +1,38 @@
+source("class_def.R")
+
+# Testing for simMetric
+
+## to ensure call works
+SM1 = simMetric(c(1,1,1,0), c(1,1,0,1), c(0.1,0.2,0.3,0.4), 1, c(0.1,0.2,0.3,0.4))
+SM2 = simMetric(orig.labels = c(1,1,1,0),pred.labels = c(1,1,0,1), 
+                pred.probs = c(0.1,0.2,0.3,0.4), case = 1)
+
+
+## real function calls
+
+set.seed(34)
+n = 1000
+orig.labels = rbinom(n, 1, 0.2)
+pred.labels = rbinom(n, 1, 0.2)
+
+# make realistic pred.probs
+pred.probs= rep(0, n)
+for(i in 1:n){
+    pred.probs[i] = ifelse(pred.labels[i] == 1, 
+                           rnorm(1,0.75, 0.2), 
+                           rnorm(1,0.25, 0.2))
+}
+
+pred.probs[pred.probs < 0] = 0.05
+pred.probs[pred.probs > 1] = 0.95
+
+case = 1
+
+SM3 = simMetric(orig.labels, pred.labels, pred.probs, case)
+SM3$auc = auc(SM3)
+SM3$f1 = f1(SM3)
+
+bins = seq(0.05,0.2,0.05)
+SM4 = simMetric(orig.labels, pred.labels, pred.probs, case, bins)
+SM4$auc = auc(SM4)
+SM4$f1 = f1(SM4)
