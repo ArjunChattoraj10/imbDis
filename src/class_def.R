552a64f4e5346fc0b415455db32f04b727b91751
Arjun Chattoraj <arjunchattoraj10@gmail.com>
2020-08-24 00:07:55 -0400

    Metric includes a generalized calculation, requires testing

--- src/class_def.R
+++ src/class_def.R
@@ -198,3 +198,70 @@ brier.simMetric = function(obj){
     return(data.frame(bins, brier, n_samps))
 }
 
+manual_loss = function(obj) UseMethod("manual_loss")
+munual_loss.simMetric = function(obj, f){
+    # framework of metric calculation. 
+    # performs all necessary pre-processing and
+    # obtains the result of the provided discrimination metric for every loop
+    
+    # f must be a function that takes in arguments in the form f(labels, probs)
+    # and must return a single value.
+    
+    # define variables to reduce code clutter
+    probs = obj$probs
+    bins = obj$bins
+    labels_01 = obj$labs_01 # necessary for proper functionality of roc()
+    samp_size = obj$sample.size
+    cases_list = obj$bin.caseSizes
+    
+    ## subset labels and probs into only case and only not case
+    
+    ### indices with case label
+    ### ensures correspondence of labels and probabilities after subsetting
+    case_i = which(labels_01 == 1)
+    
+    ### subset labels and probs for case
+    labels_case = labels_01[case_i]
+    probs_case = probs[case_i]
+    
+    ### subset labels and prons for not case
+    labels_control = labels_01[-case_i]
+    probs_control = probs[-case_i]
+    
+    ## compute returned values
+    metric = rep(0, length(bins))
+    n_samps = rep(0, length(bins))
+    
+    for(i in 1:length(bins)){
+        
+        # set up ROC calculations
+        n_cases = cases_list[i]
+        n_control = samp_size - n_cases
+        
+        # random sample of indices for case and non-case labels and probabilities
+        sub_i_case = sample(length(probs_case), n_cases)
+        sub_i_control = sample(length(probs_control), n_control)
+        
+        # labels and probabilities organized as all elements corresponding to case
+        # followed by all elements corresponding to control
+        labs = c(labels_case[sub_i_case], labels_control[sub_i_control])    
+        probs = c(probs_case[sub_i_case], probs_control[sub_i_control])
+        
+        # shuffle labels and predictions
+        # same indices used to subset ensures correspondence of 
+        # labels and predictions after shuffling
+        i_test = sample(length(labs))
+        labs = labs[i_test]
+        probs = probs[i_test]
+        
+        # calculate metric value and save results
+        metric[i] = f(labs, probs)
+        n_samps[i] = length(probs)
+    }
+    
+    # return data frame
+    return(data.frame(bins, metric, n_samps))
+}
+
+
+
