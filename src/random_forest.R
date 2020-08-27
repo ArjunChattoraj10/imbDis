cba9fd5e011f9bad9f575c1a95a45b2c3d2b5208
Arjun Chattoraj <arjunchattoraj10@gmail.com>
2020-08-27 03:44:12 -0400

    updating test files

---
+++ src/random_forest.R
@@ -0,0 +1,41 @@
+# Random Forest Classification
+
+# Ensure presence of file dependencies
+source("src/train_test.R")
+
+library(caret)
+library(imbDis)
+
+set.seed(225)
+
+# random forest with 7 predictors
+tc = trainControl(method = "cv", number = 10)
+RF_7 = train(factor(y) ~ x1+x2+x3+x4+x5+x6+x7, data = train_dat, method = "rf", trControl = tc)
+
+# random forest with all predictors
+RF_all = train(factor(y) ~ ., data = train_dat, method = "rf", trControl = tc)
+
+# obtain predicted probabilities and labels - 7 preds
+preds_RF_7 = predict(RF_7$finalModel, test_dat, type = "prob")[,2]
+
+# obtain predicted probabilities and labels - all preds
+preds_RF_all = predict(RF_all$finalModel, test_dat, type = "prob")[,2]
+
+# save the files to csv
+res_RF = data.frame(test_dat$y, preds_RF_7, preds_RF_all)
+names(res_RF)[1] = "orig"
+write.csv(res_RF, "data/res_RF.csv", row.names = FALSE)
+
+# Define classes
+SM_RF_7 = simMetric(test_dat$y, preds_RF_7, 1)
+SM_RF_all = simMetric(test_dat$y, preds_RF_all, 1)
+
+# c-statistic and F1 for 7 preds RF
+auc.simMetric(SM_RF_7)
+brier.simMetric(SM_RF_7)
+
+# c-statistic and F1 for all preds RF
+auc.simMetric(SM_RF_all)
+brier.simMetric(SM_RF_all)
+
+
