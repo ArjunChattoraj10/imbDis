13d721ecbaaca93a655d82184eb92cfcaaca734f
Arjun Chattoraj <arjunchattoraj10@gmail.com>
2020-08-27 12:14:53 -0400

    package diagnostics

--- utils/train_test.R
+++
@@ -1,9 +0,0 @@
-# Creating Train/test split
-
-sim_dat = read.csv("data/simulated_data_arjun.csv")
-
-set.seed(343)
-train_i = sample(5000, 3500)
-
-train_dat = sim_dat[train_i,]
-test_dat = sim_dat[-train_i,]
